37d2f6ff511d5e0dc2e30b90f01a5cc289ca3995
S Rupesh <wrufesh@gmail.com>
2025-05-24 17:33:46 +0200

    minor fix

--- tif_to_cog_converter/main.py
+++ tif_to_cog_converter/main.py
@@ -58,7 +58,7 @@ for input_tif in files:
         source_crs = os.environ.get('INPUT_FILE_CRS') 
 
         if source_crs is None:
-            source_crs = src.crs
+            source_crs = src.crs.to_string()
         
         if source_crs is None:
             raise ValueError("CRS is neither in the file nor provided as an environment variable.")
