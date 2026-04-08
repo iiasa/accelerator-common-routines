7a8e35039f0154d2c071a6a896934b3dca9d61f7
S Rupesh <wrufesh@gmail.com>
2026-04-08 10:59:25 +0200

    fix regional validator input

--- csv_regional_timeseries_validator/main.py
+++ csv_regional_timeseries_validator/main.py
@@ -8,7 +8,7 @@ filepaths = os.environ.get('selected_filenames', '').split(',')
 
 for filepath in filepaths:
     
-    print(f"_____________Validating file: {file} _____________")
+    print(f"_____________Validating file: {filepath} _____________")
 
     csv_regional_timeseries_verification_service = CsvRegionalTimeseriesVerificationService(
         filename=f"inputs/{filepath.split('/')[-1]}",
@@ -20,4 +20,4 @@ for filepath in filepaths:
 
     csv_regional_timeseries_verification_service()
 
-    print(f"_____________DONE: Validating file: {file} _____________")
+    print(f"_____________DONE: Validating file: {filepath} _____________")
